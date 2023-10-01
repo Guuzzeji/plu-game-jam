fb4feb65defbb66971d97250de895c35735cc391
Guuzzeji <5lh8dl440f@gmail.com>
2023-10-01 00:10:27 -0700

    Creating bullet resource
    NOTE: Important to know that resources cannot have references to Scene that are using the resource.

---
+++ src/bullet_resource.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name Bullet_Type
+
+@export var Name: String
+@export var Damage: int
+@export var Speed: int
+@export var Projectile: PackedScene
+@export var Ammo: PackedScene
