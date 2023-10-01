afbe6b81d0d00c2f53caa6e3007e1a573414565d
Guuzzeji <5lh8dl440f@gmail.com>
2023-09-30 21:40:04 -0700

    Renaming and clean up
    Cleaning up how bullet code looks

--- componets/bullets/bullet.gd
+++ componets/bullets/bullet.gd
@@ -1,19 +1,19 @@
 extends Area3D
 
-var velecity
+@export var Bullet_Speed: int = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_as_top_level(true)
-	pass # Replace with function body.
+	set_as_top_level(true) # Sets node to the view top of level tree
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
-	position += -global_transform.basis.z * 50 * delta
+	position += -global_transform.basis.z * Bullet_Speed * delta
 	pass
 
-
+# Use when ever enemy node goes into body
 func _on_body_entered(body):
 	queue_free()
-	pass # Replace with function body.
+	pass
