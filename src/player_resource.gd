9b4b051ff9bf472863c285760d0a81202ea9b0ec
Guuzzeji <5lh8dl440f@gmail.com>
2023-10-03 09:43:54 -0700

    Remove name and other stuff in player data

--- src/player_resource.gd
+++ src/player_resource.gd
@@ -2,9 +2,8 @@ extends Resource
 
 class_name Player_Data
 
-@export var Name: String
-@export var Health: int
-@export var Mania: int
+@export var Health: int = 100
+@export var Mania: int = 100
 @export var Bullet_Inventory: Array[PackedScene]
 @export var Right_Barrel: PackedScene
 @export var Left_Barrel: PackedScene
