0d13176b6f6a6acf0bf250d92ad5fbd05fa97d7f
Gabe <46883697+Guuzzeji@users.noreply.github.com>
2023-10-21 05:35:37 -0700

    Adding bullet labels to UI
    Adding simple labels to tell player what bullet they are current shooting and for which barrel of the gun

--- componets/player/player_hud.gd
+++ componets/player/player_hud.gd
@@ -14,24 +14,19 @@ func _process(delta):
 	$HealthBar.value = PlayerInfo.Health
 	$ManaBar.value = PlayerInfo.Mana
 	barrel_bullet_switch()
+	crosshair_bullet_icon()
 	pass
 	
 func barrel_bullet_switch():
-	
-	
-	
-	if Input.is_action_pressed("Left_barrel_type"):
-		$BulletPanel.visible = true
-		if Input.is_action_just_pressed("Scroll_barrel_down") and  PlayerInfo.Bullet_Inventory.size() != 0:
-			pass
-		elif  Input.is_action_just_pressed("Scroll_barrel_up") and  PlayerInfo.Bullet_Inventory.size() != 0:
-			pass
-			
-	elif Input.is_action_pressed("Right_barrel_type"):
-		$BulletPanel.visible = true
-		if Input.is_action_just_pressed("Scroll_barrel_down") and  PlayerInfo.Bullet_Inventory.size() != 0:
-			pass
-		elif  Input.is_action_just_pressed("Scroll_barrel_up") and  PlayerInfo.Bullet_Inventory.size() != 0:
-			pass
-	else:
-		$BulletPanel.visible = false
+	if Input.is_action_pressed("Left_barrel_type") and PlayerInfo.Bullet_Inventory.size() != 0:
+		$BulletLeft.text = PlayerInfo.Bullet_Inventory[PlayerInfo.Inv_Index_Left_Barrel].Name
+	elif Input.is_action_pressed("Right_barrel_type") and PlayerInfo.Bullet_Inventory.size() != 0:
+		$BulletRight.text = PlayerInfo.Bullet_Inventory[PlayerInfo.Inv_Index_Right_Barrel].Name
+
+func crosshair_bullet_icon():
+	if PlayerInfo.Left_Barrel != null:
+		$BulletLeft.text = PlayerInfo.Left_Barrel.Name
+		
+	if PlayerInfo.Right_Barrel != null:
+		$BulletRight.text = PlayerInfo.Right_Barrel.Name
+	pass 
