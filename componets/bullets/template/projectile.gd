c368cf745dacb6ceb658229e4ca75e9b36febf2b
Gabe <46883697+Guuzzeji@users.noreply.github.com>
2024-01-05 19:13:41 -0800

    Adding notes regarding how to use as template

--- componets/bullets/template/projectile.gd
+++ componets/bullets/template/projectile.gd
@@ -1,5 +1,15 @@
 extends Area3D
 
+# **How to use**
+# Create a new node .tscn file and have the root node be a Area3d node
+# Then attach this script onto the Area3d node you create for the scene
+# Also create a Timer parent node with the scene you created, will have to attach that in properties menu under Life Timer
+# After that add a collision shape and fill out the properties in the editor
+
+# Notes:
+# - If you are getting error: "E 0:00:00:0655   projectile.gd:15 @ _ready(): Signal 'timeout' is already connected to given callable 'Area3D(projectile.gd)::_on_life_timer_timeout' in that object."
+#		Just ignore it for now, Will fix later and isn't causing any bugs as of now 
+
 @export var Bullet_Info : Bullet_Type
 @export var Player_Info : Player_Data
 @export var Life_Timer : Timer
@@ -14,7 +24,6 @@ func _ready():
 	set_as_top_level(true) # Sets node to root level of the node tree (basically at sets parent to level/world node)
 	Life_Timer.timeout.connect(_on_life_timer_timeout) # Connecting singal (time out) to func
 	Life_Timer.one_shot = true
-	print(Bullet_Info)
 	Life_Timer.start(Bullet_Info.Life_Time)
 	pass
 
