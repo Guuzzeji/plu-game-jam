c72074112304635cd9bea14f0c7fbe23d798dacc
FlamingGears <146592921+FlamingGears@users.noreply.github.com>
2024-02-23 15:43:08 -0800

    tested_importing/protoLevel
    1m cube onshape = 1m cube godot
    no scale issues so far!

--- componets/Level_Devices/One_Way_Teleporter/one_way_exit.gd
+++ componets/Level_Devices/One_Way_Teleporter/one_way_exit.gd
@@ -1,10 +1,12 @@
 extends Node3D
 @export var One_Way_TeleChannel : int
+@export var invisible = false	## hide unless for debugging
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_visible(!invisible)	## for decorating level starts
 	add_to_group("Exit_Teleport_Points")
-	pass # Replace with function body.
+	pass # Replace with function body.W
 
 
 
